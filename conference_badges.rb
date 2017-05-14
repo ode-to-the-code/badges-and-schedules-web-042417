b1a7a6e6fef8650ce558b40a610ab002c697417e
ode-to-the-code <justcallmethis@gmail.com>
2017-05-13 21:39:23 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+
+def assign_rooms(list)
+  list.each_with_index.map do |name, index|
+     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+end
+
+def printer(array)
+  batch_badge_creator(array).map do |name|
+    puts  "#{name}"
+    end
+  # assign_rooms(array).each_with_index.map do |name, index|
+  #    puts "#{name}"
+  # end
+  assign_rooms(array).each.map do |name|
+     puts "#{name}"
+  end
+end
